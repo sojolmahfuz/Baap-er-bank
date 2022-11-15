4d273167eb19c43e86a1d2ded105bc25ce80eb93
sojolmahfuz <sojol.cu@gmail.com>
2022-11-15 13:15:03 +0600

    js applied

--- js/banking.js
+++ js/banking.js
@@ -6,5 +6,13 @@ document.getElementById('deposit-button').addEventListener('click', function(){
     const depositAmount = depositInput.value;
 
     console.log(depositAmount);
+
+    const depositTotal = document.getElementById('deposit-total');
+
+    depositTotal.innerText = depositAmount;
+
+    //Clear the input field
+
+    depositInput.value ='';
 })
 
