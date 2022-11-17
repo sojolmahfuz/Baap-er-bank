362951955abd89eb3fb5b2582c5038cc8a970a24
sojolmahfuz <sojol.cu@gmail.com>
2022-11-17 12:05:29 +0600

    updated

--- js/banking.js
+++ js/banking.js
@@ -3,13 +3,29 @@
 document.getElementById('deposit-button').addEventListener('click', function(){
     const depositInput = document.getElementById('deposit-amount');
 
-    const depositAmount = depositInput.value;
+    const newDepositAmountText = depositInput.value;
+    const newDepositAmount = parseFloat(newDepositAmountText);
 
-    console.log(depositAmount);
 
     const depositTotal = document.getElementById('deposit-total');
 
-    depositTotal.innerText = depositAmount;
+    const previousDepositText = depositTotal.innerText;
+    const previousDepositAmount = parseFloat(previousDepositText);
+
+    const newDepositTotal = previousDepositAmount  + newDepositAmount;
+
+    depositTotal.innerText = newDepositTotal;
+
+
+    // Update Account Balance
+
+    const balanceTotal = document.getElementById('balance-total');
+    
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+
+    const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+    balanceTotal.innerText = newBalanceTotal;
 
     //Clear the input field
 
